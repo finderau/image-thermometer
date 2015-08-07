90402eee0f9d35235365f2877c88e1cab05bc92a
Ipek Turgul <ipek.turgul@hiveempire.com>
2015-08-07 15:35:59 +1000

    Reorganize files

--- replace-css.js
+++ replace-css.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 var css;
 
-fs.readFile('./dist/css/style.min.css', function (err, data) {
+fs.readFile('./src/build/css/style.min.css', function (err, data) {
   if (err) throw err;
   css = data.toString();
 });
 
-fs.readFile('./js/build/all.js', function (err, data) {
+fs.readFile('./src/build/js/all.js', function (err, data) {
   if (err) throw err;
   var js_content = data.toString();
   
   var result = js_content.replace(/CSS STRING/g, css);
-  fs.writeFile('./js/build/all.js', result, 'utf8', function (err) {
+  fs.writeFile('./src/build/js/all.js', result, 'utf8', function (err) {
      if (err) throw err;
   });
 });
