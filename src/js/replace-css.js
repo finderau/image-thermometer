69bd1843170e457ff265c5ba03a269123b65db47
Ipek Turgul <ipek.turgul@hiveempire.com>
2015-08-10 09:54:28 +1000

    Add jshint to build scripts

--- src/js/replace-css.js
+++ src/js/replace-css.js
@@ -4,14 +4,16 @@ var css;
 fs.readFile('./src/build/css/style.min.css', function (err, data) {
   if (err) throw err;
   css = data.toString();
+
+  fs.readFile('./src/build/js/all.js', function (err, data) {
+    if (err) throw err;
+    var js_content = data.toString();
+    
+    var result = js_content.replace(/CSS STRING/g, css);
+    fs.writeFile('./src/build/js/all.js', result, 'utf8', function (err) {
+     if (err) throw err;
+   });
+  });
+
 });
 
-fs.readFile('./src/build/js/all.js', function (err, data) {
-  if (err) throw err;
-  var js_content = data.toString();
-  
-  var result = js_content.replace(/CSS STRING/g, css);
-  fs.writeFile('./src/build/js/all.js', result, 'utf8', function (err) {
-     if (err) throw err;
-  });
-});
